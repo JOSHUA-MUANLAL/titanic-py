f67c4329bb912546d145d943e6f10800b9780ea6
JOSHUA-MUANLAL <105428544+JOSHUA-MUANLAL@users.noreply.github.com>
2023-08-18 20:43:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+import numpy as np
+import titanic as tic
+
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict',methods=["POST"])
+def play():
+    pclass=int(request.form['pclass'])
+    sex=int(request.form["gender"])
+    age=int(request.form['age'])
+    sibsp=int(request.form['sibsp'])
+    parch=float(request.form['parch'])
+    fare=float(request.form['fare'])
+    embark=int(request.form['embarked'])
+    
+    data=np.array([[pclass,sex,age,sibsp,parch,fare,embark]])
+    t=tic.titanic()
+    result=t.entry(data)
+    return render_template('index.html',result=result)
+
+
+if __name__ == '__main__':  
+   app.run(debug = True) 
+    
+    
+    
+    
